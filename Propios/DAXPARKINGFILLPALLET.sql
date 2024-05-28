20c3f1a71c175853095d4328ee04d7d446e21b85
Roberto Castro <tonocastrom@gmail.com>
2024-05-27 22:39:33 -0600

    Se modifica vista DAXPARKINGFILLPALLET, se agregan campos y se optimiza rendimiento

--- Propios/DAXPARKINGFILLPALLET.sql
+++ Propios/DAXPARKINGFILLPALLET.sql
@@ -1,12 +1,18 @@
 IF OBJECT_ID('dbo.DAXPARKINGFILLPALLET', 'V') IS NOT NULL
 DROP VIEW dbo.DAXPARKINGFILLPALLET GO
 CREATE VIEW [dbo].[DAXPARKINGFILLPALLET] AS
-SELECT isnull(
-                (SELECT (T1.INVENTQTYAVAILPHYSICAL/AVG(tmp.QtyConvert01ByUnit))*100 AS QtyConvert01ByUnit
-                 FROM daxinventonhandcontainer tmp
-                 WHERE tmp.shipcpycontainerrecid = t1.shipcpycontainerrecid
-                   AND tmp.wmslocationid = t1.wmslocationid
-                 GROUP BY tmp.shipcpycontainerrecid, tmp.wmslocationid
-                 HAVING AVG(tmp.QtyConvert01ByUnit) > 0),0) AS PERCENTFILLPALLET,
+SELECT iif(t2.QtyConvert01ByUnitAvg > 0, (t2.INVENTQTYAVAILPHYSICAL / t2.QtyConvert01ByUnitAvg) * 100, 0) AS PERCENTFILLPALLET,
+       iif(t2.QtyConvert01ByUnitAvg > 0, (t1.INVENTQTYAVAILPHYSICAL / t2.QtyConvert01ByUnitAvg) * 100, 0) AS PERCENTFILLITEMPALLET,
+       t2.ITEMSINPALLET,
        t1.*
 FROM daxinventonhandcontainer t1
+INNER JOIN
+  (SELECT tmp.shipcpycontainerrecid,
+          tmp.wmslocationid,
+          SUM(tmp.INVENTQTYAVAILPHYSICAL) AS INVENTQTYAVAILPHYSICAL,
+          isnull(AVG(tmp.QtyConvert01ByUnit), 0) AS QtyConvert01ByUnitAvg,
+          COUNT(*) AS ITEMSINPALLET
+   FROM daxinventonhandcontainer tmp
+   GROUP BY tmp.shipcpycontainerrecid,
+            tmp.wmslocationid) AS t2 ON t1.shipcpycontainerrecid = t2.shipcpycontainerrecid
+AND t1.wmslocationid = t2.wmslocationid
