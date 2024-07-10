5c8312f058d21ed517dfa11989101ee8fe9940ad
Roberto Castro <tonocastrom@gmail.com>
2024-07-10 00:35:31 -0600

    Se modifica vistas DAXINVENT_TRANSFERJOURNAL Y DAXINVENTSUMINVENTDIMVIEW para que no muestre los articulos bloqueados

--- General/Desarrollo/ObtenerVistas.sql
+++ General/Desarrollo/ObtenerVistas.sql
@@ -36,3 +36,18 @@
  )
  AND name NOT IN ('ReportingVirtualDataAreaView','ReportingVirtualDataAreaView1','ReportingVirtualDataAreaView2')
  order by name
+
+
+
+
+-- Ver la definicion de una vista en especifico
+SELECT name,
+ CONCAT('IF EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.VIEWS  WHERE TABLE_NAME = ''',name,''') DROP VIEW ',name,CHAR(10),' GO ',char(10),
+ (
+	 SELECT definition
+	FROM sys.sql_modules  
+	WHERE object_id = OBJECT_ID(name)
+ ),CHAR(10)) as query
+ FROM sysobjects 
+ WHERE xtype = 'V'
+ AND name = 'DAXINVENT_TRANSFERJOURNAL'
