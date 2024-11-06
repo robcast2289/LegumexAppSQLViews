4a873bc0d98ecbb58cf5170275ef07c0811d7a6b
DynamicsAdmin <DynamicsAdmin@gdynamics.com>
2024-11-06 14:57:25 -0600

    Se actualizan las vistas y se agregan los SP de ambiente de desarrollo, Se crea backup del ambiente de Pruebas antes de actualizacion

--- General/Desarrollo/ObtenerVistas.sql
+++ General/Desarrollo/ObtenerVistas.sql
@@ -25,8 +25,8 @@
 	 SELECT definition
 	FROM sys.sql_modules  
 	WHERE object_id = OBJECT_ID(name)
- ),
- CHAR(10)  --Quitar
+ )
+ ,CHAR(10)  --Quitar
  ,'GO') as query
  FROM sysobjects 
  WHERE xtype = 'V'  -- 'P' Para StoreProcedures
@@ -53,3 +53,40 @@ SELECT name,
  FROM sysobjects 
  WHERE xtype = 'V'
  AND name = 'DAXINVENT_TRANSFERJOURNAL'
+
+
+
+
+
+
+ ----------- PROCEDIMIENTOS ----------
+
+  -- Listar con comando de Eliminar y crear de los procedimientos creadas cuera de Dynamics
+ SELECT name,
+ CONCAT('IF EXISTS (SELECT * FROM sys.objects WHERE type = ''P'' AND name = ''',name,''') DROP PROCEDURE ',name,CHAR(10),'GO',char(10),
+ (
+	 SELECT definition
+	FROM sys.sql_modules  
+	WHERE object_id = OBJECT_ID(name)
+ )
+ ,'GO') as query
+ FROM sysobjects 
+ WHERE xtype = 'P'  -- 'P' Para StoreProcedures
+ AND name NOT IN (
+ SELECT NAME FROM SQLDICTIONARY
+ WHERE FIELDID = 0
+ AND FLAGS = 1
+ )
+ AND name NOT IN (
+ 'CREATESERVERSESSIONS',
+ 'CREATEUSERSESSIONS',
+ 'sp_WHSOnHand',
+ 'sp_WHSOnHandWithDelta',
+ 'getNumInternal',
+ 'MaintainShipCarrierRole',
+ 'sp_AddBIAnalysisServer',
+ 'sp_AddReportServer',
+ 'SP_ConfigureTablesForChangeTracking',
+ 'sp_GetNextRecId',
+ 'sp_PreCloseNonFinTransfer')
+ order by name
